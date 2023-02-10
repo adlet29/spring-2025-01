9a9616759f19ad0e81d06ee31051947ed25aa936
Adilet Assanov <adilet.assanov@mkb.kz>
2023-02-10 11:20:30 +0600

    Получить раннер из контекста

--- spring-01-homework/src/main/java/kz/almaty/spring/Main.java
+++ spring-01-homework/src/main/java/kz/almaty/spring/Main.java
@@ -1,15 +1,14 @@
 package kz.almaty.spring;
 
 import kz.almaty.spring.service.ApplicationRunner;
-import kz.almaty.spring.service.QuestionService;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/spring-context.xml");
-        QuestionService questionService = context.getBean(QuestionService.class);
-        new ApplicationRunner(questionService).run();
+        ApplicationRunner applicationRunner = context.getBean(ApplicationRunner.class);
+        applicationRunner.run();
     }
 
 }
