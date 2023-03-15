29fd1c8a4480a10b35d18a6451c65730d3ac2a97
Adilet Assanov <adilet.assanov@mkb.kz>
2023-03-15 17:45:27 +0600

    junit + mockito

--- spring-02-homework/src/test/java/kz/almaty/spring/dao/QuestionDaoFileCsvTest.java
+++ spring-02-homework/src/test/java/kz/almaty/spring/dao/QuestionDaoFileCsvTest.java
@@ -1,36 +1,21 @@
 package kz.almaty.spring.dao;
 
-import kz.almaty.spring.exceptions.FileNameIllegalArgumentException;
-import kz.almaty.spring.exceptions.ReaderRuntimeException;
+import kz.almaty.spring.model.Question;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("Класс QuestionDaoFileCsv")
 public class QuestionDaoFileCsvTest {
     @DisplayName("Тестовая загрузка файла CSV")
     @Test
-    void loadCSVFile() {
-        String fileName = "test.csv";
-        ClassLoader classLoader = getClass().getClassLoader();
-        try (InputStream inputStream = classLoader.getResourceAsStream(fileName)) {
-            if (inputStream == null) {
-                throw new FileNameIllegalArgumentException("file not found! " + fileName);
-            }
-            InputStreamReader streamReader = new InputStreamReader(inputStream);
-            BufferedReader reader = new BufferedReader(streamReader);
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-            }
-
-        } catch (IOException e) {
-            throw new ReaderRuntimeException("problems reading the file!");
+    void findAll() {
+        QuestionDaoFileCsv questionDaoFileCsv = new QuestionDaoFileCsv("test.csv");
+        var questions = questionDaoFileCsv.findAll();
+        assertThat(questions).isNotEmpty();
+        for (Question question : questions) {
+            assertThat(question.getOptionList()).isNotEmpty();
         }
     }
 }
--- spring-02-homework/src/test/java/kz/almaty/spring/service/QuestionServiceImplTest.java
+++ spring-02-homework/src/test/java/kz/almaty/spring/service/QuestionServiceImplTest.java
@@ -1,16 +1,21 @@
 package kz.almaty.spring.service;
 
 import kz.almaty.spring.dao.QuestionDao;
-import kz.almaty.spring.dao.QuestionDaoFileCsv;
+import kz.almaty.spring.model.Option;
 import kz.almaty.spring.model.Question;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @DisplayName("Класс QuestionServiceImpl")
 @ExtendWith(MockitoExtension.class)
@@ -19,12 +24,19 @@ public class QuestionServiceImplTest {
     @Mock
     private QuestionDao questionDao;
 
-    private QuestionService questionService;
+    @InjectMocks
+    private QuestionServiceImpl questionService;
 
     @BeforeEach
     void setUp() {
-        questionDao = new QuestionDaoFileCsv("test.csv");
-        questionService = new QuestionServiceImpl(questionDao);
+        List<Question> questions = new ArrayList<>();
+        List<Option> options = new ArrayList<>();
+        options.add(new Option("Option1", 1, false));
+        options.add(new Option("Option2", 2, true));
+        options.add(new Option("Option3", 3, false));
+        options.add(new Option("Option4", 4, false));
+        questions.add(new Question("Question", options));
+        when(questionDao.findAll()).thenReturn(questions);
     }
 
     @DisplayName("Получить вопросы теста для студентов")
