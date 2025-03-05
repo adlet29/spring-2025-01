6411c8f683eb30b6f6b4b614b55bcbd6c12cfcf3
adlet <adletassanov26@gmail.com>
2025-03-05 23:38:58 +0500

    Доделать локализацию

--- spring-shell/src/main/java/ru/otus/hw/Application.java
+++ spring-shell/src/main/java/ru/otus/hw/Application.java
@@ -1,11 +1,15 @@
 package ru.otus.hw;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
+import ru.otus.hw.config.AppProperties;
 import ru.otus.hw.service.TestRunnerService;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
 public class Application {
     public static void main(String[] args) {
 
