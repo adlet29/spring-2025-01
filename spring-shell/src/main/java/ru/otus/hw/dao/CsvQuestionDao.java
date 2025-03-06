875d76bac1fb6cd32ed34e93a7fb054867603910
Адилет Асанов <adilet.assanov@mkb.kz>
2025-03-06 15:26:35 +0500

    Получить вопросы и варианты

--- spring-shell/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ spring-shell/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -1,10 +1,18 @@
 package ru.otus.hw.dao;
 
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,11 +23,38 @@ public class CsvQuestionDao implements QuestionDao {
 
     @Override
     public List<Question> findAll() {
-        // Использовать CsvToBean
-        // https://opencsv.sourceforge.net/#collection_based_bean_fields_one_to_many_mappings
-        // Использовать QuestionReadException
-        // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
-
-        return new ArrayList<>();
+        var fileName = fileNameProvider.getTestFileName();
+        try (BufferedReader bufferedReader = getFileFromResourceAsStreamReader(fileName)) {
+            var csvToBean = getCsvToBean(bufferedReader);
+            var questionDtoList = csvToBean.parse();
+            List<Question> questions = new ArrayList<>();
+            for (QuestionDto questionDto : questionDtoList) {
+                Question question = questionDto.toDomainObject();
+                questions.add(question);
+            }
+            return questions;
+        } catch (IOException ex) {
+            throw new QuestionReadException("problems reading the file!", ex);
+        }
     }
+
+    private BufferedReader getFileFromResourceAsStreamReader(String fileName) {
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream(fileName);
+        if (inputStream == null) {
+            throw new QuestionReadException("file not found! " + fileName);
+        }
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+        return new BufferedReader(inputStreamReader);
+    }
+
+    private CsvToBean<QuestionDto> getCsvToBean(BufferedReader bufferedReader) {
+        return new CsvToBeanBuilder<QuestionDto>(bufferedReader)
+                .withType(QuestionDto.class)
+                .withIgnoreLeadingWhiteSpace(true)
+                .withSkipLines(1)
+                .withSeparator(';')
+                .build();
+    }
+
 }
