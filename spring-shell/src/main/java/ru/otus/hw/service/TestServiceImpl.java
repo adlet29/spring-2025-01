c6065bd23219228363e8d8bcf4aa8959337c563c
Адилет Асанов <adilet.assanov@mkb.kz>
2025-03-06 15:27:46 +0500

    Задать вопрос, получить ответ

--- spring-shell/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ spring-shell/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -3,6 +3,7 @@ package ru.otus.hw.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Question;
 import ru.otus.hw.domain.Student;
 import ru.otus.hw.domain.TestResult;
 
@@ -23,11 +24,39 @@ public class TestServiceImpl implements TestService {
         var questions = questionDao.findAll();
         var testResult = new TestResult(student);
 
-        for (var question: questions) {
-            var isAnswerValid = false; // Задать вопрос, получить ответ
-            testResult.applyAnswer(question, isAnswerValid);
+        for (var question : questions) {
+            int correctOption = displayQuestionWithOptions(question);
+            int studentOption = getStudentAnswer(question.answers().size());
+
+            testResult.applyAnswer(question, studentOption == correctOption);
         }
+
         return testResult;
+    }
+
+    private int displayQuestionWithOptions(Question question) {
+        ioService.printLine(question.text());
+
+        var options = question.answers();
+        int correctOption = -1;
+
+        for (int optionNumber = 1; optionNumber <= options.size(); optionNumber++) {
+            var option = options.get(optionNumber - 1);
+            ioService.printFormattedLine(optionNumber + ") " + option.text());
+
+            if (option.isCorrect()) {
+                correctOption = optionNumber;
+            }
+        }
+
+        return correctOption;
+    }
+
+    private int getStudentAnswer(int maxOption) {
+        return ioService.readIntForRangeWithPromptLocalized(
+                1, maxOption,
+                "TestService.select.correct.answer", "TestService.error.index.out.of"
+        );
     }
 
 }
