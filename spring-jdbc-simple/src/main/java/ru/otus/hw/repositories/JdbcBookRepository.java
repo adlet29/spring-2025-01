ef9866583e39085dbbf8d0767acaab1878c2cde5
Адилет Асанов <adilet.assanov@mkb.kz>
2025-03-07 17:39:54 +0500

    author and book repository fix

--- spring-jdbc-simple/src/main/java/ru/otus/hw/repositories/JdbcBookRepository.java
+++ spring-jdbc-simple/src/main/java/ru/otus/hw/repositories/JdbcBookRepository.java
@@ -1,27 +1,37 @@
 package ru.otus.hw.repositories;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
+import ru.otus.hw.models.Author;
 import ru.otus.hw.models.Book;
+import ru.otus.hw.models.Genre;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Repository
+@RequiredArgsConstructor
 public class JdbcBookRepository implements BookRepository {
+
+    private final NamedParameterJdbcOperations namedParameterJdbcOperations;
 
     @Override
     public Optional<Book> findById(long id) {
-        return Optional.empty();
+        Map<String, Object> params = Collections.singletonMap("id", id);
+
+        Book book = namedParameterJdbcOperations.queryForObject(
+                "select id, title, author_id, genre_id from books where id = :id", params, new BookRowMapper()
+        );
+        return Optional.ofNullable(book);
     }
 
     @Override
     public List<Book> findAll() {
-        return new ArrayList<>();
+        return namedParameterJdbcOperations.query("select id, title, author_id, genre_id from books", new BookRowMapper());
     }
 
     @Override
@@ -57,7 +67,11 @@ public class JdbcBookRepository implements BookRepository {
 
         @Override
         public Book mapRow(ResultSet rs, int rowNum) throws SQLException {
-            return null;
+            long id = rs.getLong("id");
+            String title = rs.getString("title");
+            long author_id = rs.getLong("author_id");
+            long genre_id = rs.getLong("genre_id");
+            return new Book(id, title, new Author(), new Genre());
         }
     }
 }
