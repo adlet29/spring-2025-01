f2e2e9b4afd3c250ff13ba487d5acb8f389df81e
Адилет Асанов <adilet.assanov@mkb.kz>
2025-04-14 19:42:39 +0500

    delete unused imports

--- spring-jpa/src/test/java/ru/otus/hw/repositories/JpaBookRepositoryTest.java
+++ spring-jpa/src/test/java/ru/otus/hw/repositories/JpaBookRepositoryTest.java
@@ -9,8 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.Import;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 import ru.otus.hw.models.Author;
 import ru.otus.hw.models.Book;
 import ru.otus.hw.models.Comment;
