8414981d88cdc8bb2d7646f38b0992dbec2f95e6
Адилет Асанов <adilet.assanov@mkb.kz>
2025-05-12 16:58:34 +0500

    spring mvc на основе spring data jpa

---
+++ spring-mvc/src/main/java/ru/otus/hw/converters/BookConverter.java
@@ -0,0 +1,39 @@
+package ru.otus.hw.converters;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import ru.otus.hw.models.Book;
+
+@RequiredArgsConstructor
+@Component
+public class BookConverter {
+    private final AuthorConverter authorConverter;
+
+    private final GenreConverter genreConverter;
+
+    private final CommentConverter commentConverter;
+
+    public String bookToString(Book book) {
+        return "Id: %d, title: %s, author: {%s}, genres: [%s]".formatted(
+                book.getId(),
+                book.getTitle(),
+                authorConverter.authorToString(book.getAuthor()),
+                genreConverter.genreToString(book.getGenre()));
+    }
+
+    public String bookWithCommentsToString(Book book) {
+        return """
+                Id: %d
+                Title: %s
+                Author: {%s}
+                Genres: [%s]
+                Comments:
+                %s
+                """.formatted(
+                book.getId(),
+                book.getTitle(),
+                authorConverter.authorToString(book.getAuthor()),
+                genreConverter.genreToString(book.getGenre()),
+                commentConverter.commentsToString(book.getComments()));
+    }
+}
